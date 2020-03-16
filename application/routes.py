e49b59c041ba4c77f36c9d7540fdb803a8c9afb7
Piotr Pawłowski <ppawlo97@gmail.com>
2020-03-16 18:55:59 +0100

    added form validation logic;

--- application/routes.py
+++ application/routes.py
@@ -1,26 +1,27 @@
 from flask import render_template
 from flask import Response
+from flask import request
 
 from application import app
 from application.Camera import Camera
 from application.forms.ModelForm import ModelForm
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     model_form = ModelForm()
+    models = {field.name: field.data for field in model_form
+                if field.type == "SelectField"}
     
-    if model_form.validate_on_submit():
-        # TODO: implement validation logic
-        pass
-
-    return render_template("index.html", model_form=model_form)
+    return render_template("index.html",
+                           model_form=model_form,
+                           models=models)
     
 
 @app.route("/camera_stream", methods=["GET"])
 def camera_stream():
+    models= eval(request.args["models"]) # TODO: implement Model class; image processing
     camera = Camera()
-    # TODO: implement Model class and model choice
     if not camera.is_available:
         return Response(camera.generate(),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
